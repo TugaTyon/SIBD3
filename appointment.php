8bf94a06b0d37e1bb60c0f5442cc24790d4d41fb
TugaTyon <j040.silva@gmail.com>
2017-11-23 00:27:00 +0000

    Add first files
    first files
    by: Barbosa & Paulo

---
+++ appointment.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+      <title>Appointment Scheduller</title>
+  </head>
+  <body>
+    <form action="insert.php" method="get">
+      <h3>Appointment Scheduller</h3>
+      <p>Please insert the Patient Name: <input type="text" name="patient_name"/></p>
+      <p><input type="submit" value="Submit"/></p>
+    </form>
+  </body>
+
+</html>
---
+++ insert.php
@@ -0,0 +1,6 @@
+<html>
+  <body>
+    <h3>Registerd Patients</h3>
+    <p>Names: <?php echo($_REQUEST['patient_name']); ?> </p>
+  </body>
+</html>
