39a5cf86705c833429dd9a2ee407902e4680240b
Rodrigo Lopes Do Ó Barbosa <rodrigo.do.o.barbosa@gmail.com>
2017-11-24 16:45:05 +0000

    Retirado lixo

--- searchPatientName.php
+++ searchPatientName.php
@@ -17,18 +17,17 @@
           echo("</p>");
           exit();
         }
-        echo($_REQUEST['patient_name']);
         $sql = "SELECT * FROM Patient WHERE name= '" . $_REQUEST['patient_name'] . "'";
         $result = $connection->query($sql);
 
-       
+
         if ($result == FALSE)
         {
           $info = $connection->errorInfo();
           echo("<p>Error: {$info[0]} {$info[1]} {$info[2]}</p>");
           exit();
         }
-        
+
         if (mysql_num_rows($result) === 0)
         {
           echo("<p>We found no Patients with such name. Would you like to add a new entry?</p>");
@@ -39,7 +38,7 @@
           echo("<table border=\"1\">");
           echo("<tr><td>ID</td><td>Name</td>
                 <td>Birthday</td><td>Address</td></tr>");
-        
+
           foreach($result as $row)
           {
             echo("<tr><td>");
@@ -56,6 +55,5 @@
         }
 
        ?>
-    <p>Names: <?php echo($_REQUEST['patient_name']); ?> </p>
   </body>
 </html>
