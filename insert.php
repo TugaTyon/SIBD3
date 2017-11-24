45b08aff4723e0ed0cf3d528f6a4d9090d9b3c27
TugaTyon <j040.silva@gmail.com>
2017-11-24 16:05:35 +0000

    updated insert

--- insert.php
+++ insert.php
@@ -1,9 +1,9 @@
 <html>
-    <head>
-      <title>Appointment Scheduller</title>
+  <head>
+    <title>Insert new patient</title>
   </head>
   <body>
-    <form action="searchPatientName.php" method="post">
+    <form action="insert.php" method="post">
       <h3>Appointment Scheduller</h3>
       <p>Please insert the Patient Name: <input type="text" name="patient_name"/></p>
       <p><input type="submit" value="Submit"/></p>
