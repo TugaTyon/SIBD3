fede78a6762b4d78a7c3af4510f86e9178d655c3
TugaTyon <j040.silva@gmail.com>
2017-11-24 14:07:11 +0000

    check for empty query response - working

--- searchPatientName.php
+++ searchPatientName.php
@@ -21,31 +21,25 @@
         $sql = "SELECT * FROM Patient WHERE name= '" . $_REQUEST['patient_name'] . "'";
         $result = $connection->query($sql);
 
+       
         if ($result == FALSE)
         {
           $info = $connection->errorInfo();
           echo("<p>Error: {$info[0]} {$info[1]} {$info[2]}</p>");
           exit();
         }
-        elseif ( !mysql_num_rows($result ))
+        
+        if (mysql_num_rows($result) === 0)
         {
           echo("<p>We found no Patients with such name. Would you like to add a new entry?</p>");
-          /** CODE TO INSERT NEW PATIENT IN THE DATABASE */
-         /* ?>
-          <form action="insert.php" method="post">
-            <h3>Insert new patient in the database</h3>
-            <p>Name: <input type="text" name="patient_name"/></p>
-            <p>Birthday: <input type="text" name="patient_birthday"/></p>
-            <p>Address: <input type="text" name="patient_address"/></p>
-            <p><input type="submit" value="Submit"/></p>
-          </form>
-          <?php*/
+         
         }
         else
         {
           echo("<table border=\"1\">");
           echo("<tr><td>ID</td><td>Name</td>
                 <td>Birthday</td><td>Address</td></tr>");
+        
           foreach($result as $row)
           {
             echo("<tr><td>");
@@ -59,8 +53,9 @@
             echo("</td></tr>");
           }
           echo("</table>");
+        }
 
-      ?>
+       ?>
     <p>Names: <?php echo($_REQUEST['patient_name']); ?> </p>
   </body>
 </html>
