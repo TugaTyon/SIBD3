1e46fae14d882513d900682717d5c652ca33c576
Rodrigodoo <rodrigo.do.o@tecnico.ulisboa.pt>
2017-11-25 23:32:14 +0000

    Add files via upload

---
+++ EX2/useful_shit.php
@@ -0,0 +1,21 @@
+
+
+
+<p><a href="session2.php">Go to page 2</a></p>
+
+
+/*
+         ?>
+          <html>
+            <body>
+            <form action="insert.php" method="post">
+              <h3>Insert new patient in the database</h3>
+              <p>Name: <input type="text" name="patient_name"/></p>
+              <p>Birthday: <input type="text" name="patient_birthday"/></p>
+              <p>Address: <input type="text" name="patient_address"/></p>
+              <p><input type="submit" value="Submit"/></p>
+            </form>
+            </body>
+          </html>
+          <?php
+*/
