842b6880516952b4fa56987feb2769003895df62
Rodrigo Lopes Do Ó Barbosa <rodrigo.do.o.barbosa@gmail.com>
2017-11-26 01:48:58 +0000

    Prints all the devices worn by patient - NO ORDER

--- EX2/devicesByPatient.php
+++ EX2/devicesByPatient.php
@@ -3,9 +3,47 @@
       <title>Devices by Patient</title>
   </head>
   <body>
-  
-      <h3>Devices by Patient</h3>
-      <h4><?php echo($_REQUEST['patient_name']); ?></h4>
+      <h3>Patient: <?php echo($_REQUEST['patient_name']); ?></h3>
+      <h4>Devices</h4>
+      <?php
+      try
+      {
+        $host = 'db.tecnico.ulisboa.pt';
+        $user = 'ist173065';
+        $pass = 'wsfv4254';
+        $dsn = "mysql:host=$host;dbname=$user";
+        $connection = new PDO($dsn, $user, $pass);
+      }
+        catch(PDOException $exception)
+      {
+        echo("<p>Error: ");
+        echo($exception->getMessage());
+        echo("</p>");
+        exit();
+      }
+      $sql = "SELECT * FROM  Wears , Device
+      WHERE patient = ".$_REQUEST['patient_id']." AND snum = serialnum AND manuf = manufacturer ";
+      $stmt= $connection->prepare($sql);
+      $stmt-> execute();
+
+      echo("<table border=\"1\">");
+      echo("<tr><td>ID</td><td>Model</td>
+            <td>Manufacturer</td></tr>");
+
+      foreach($stmt as $row)
+      {
+        echo("<tr><td>");
+        echo($row['snum']);
+        echo("</td><td>");
+        echo($row['model']);
+        echo("</td><td>");
+        echo($row['manufacturer']);
+        echo("</td><tr>");
+
+      }
+      echo("</table>");
+
+       ?>
 
 
   </body>
