f8a189be2a98d2082c107ca5c009dbfb7a7af255
Rodrigo Lopes Do Ó Barbosa <rodrigo.do.o.barbosa@gmail.com>
2017-11-27 18:26:56 +0000

    Only Final Replacement Missing

---
+++ EX2/confirmReplacement.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+      <title>Appointment Scheduller</title>
+  </head>
+  <body>
+    <form action="searchPatientName.php" method="post">
+      <h3>Appointment Scheduller</h3>
+      <p>Please insert the Patient Name: <input type="text" name="patient_name"/></p>
+      <p><input type="submit" value="Submit"/></p>
+    </form>
+  </body>
+
+</html>
--- EX2/deviceReplacement.php
+++ EX2/deviceReplacement.php
@@ -1,13 +1,61 @@
 <html>
     <head>
-      <title>Appointment Scheduller</title>
+      <title>Available Devices</title>
   </head>
   <body>
-    <form action="searchPatientName.php" method="post">
-      <h3>Appointment Scheduller</h3>
-      <p>Please insert the Patient Name: <input type="text" name="patient_name"/></p>
-      <p><input type="submit" value="Submit"/></p>
-    </form>
-  </body>
+      <h3>Available Devices: <?php echo($_REQUEST['manuf']); ?></h3>
 
+      <?php
+      try
+      {
+        $host = 'db.tecnico.ulisboa.pt';
+        $user = 'ist173065';
+        $pass = 'wsfv4254';
+        $dsn = "mysql:host=$host;dbname=$user";
+        $connection = new PDO($dsn, $user, $pass);
+      }
+        catch(PDOException $exception)
+      {
+        echo("<p>Error: ");
+        echo($exception->getMessage());
+        echo("</p>");
+        exit();
+      }
+      $sql = "SELECT * FROM  Wears , Device
+      WHERE snum = serialnum AND manuf = manufacturer AND manuf = '".$_REQUEST['manuf']."' GROUP BY serialnum";
+      $stmt= $connection->prepare($sql);
+      $stmt-> execute();
+
+      date_default_timezone_set('Europe/Lisbon');
+      $currentDate = date('Y-m-d H:i:s');
+
+      echo ("<h4>Choose a Device for Replacement</h4>");
+      echo("<table border=\"1\">");
+      echo("<tr><td>ID</td><td>Model</td>
+            <td>Manufacturer</td></tr>");
+
+      foreach($stmt as $row)
+      {
+        if (!($currentDate < $row['end'])) {//TIRAR COMENTARIO DEPOIS DE TABELAS UPDATED!!!!!!!!!!!
+          echo("<tr><td>");
+          echo($row['snum']);
+          echo("</td><td>");
+          echo($row['model']);
+          echo("</td><td>");
+          echo($row['manufacturer']);
+          echo("</td><td>");
+          ?>
+          <form name="form" method="POST" action="confirmReplacment.php">
+            <input value="<?php echo($row['manufacturer']);?>" type="hidden" name="manuf">
+            <input type="submit"  value="Choose">
+           </form>
+          <?php
+          echo("</td></tr>");
+        }
+      }
+      echo("</table>");
+
+       ?>
+
+  </body>
 </html>
