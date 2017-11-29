e4a356e7f77737ee381d7f932389a39188d2b463
TugaTyon <j040.silva@gmail.com>
2017-11-29 01:37:08 +0000

    added gitignore and dropdown menu for Devices and models (dynamic, accepting user input through jquery)

--- EX3/newStudy.php
+++ EX3/newStudy.php
@@ -7,16 +7,12 @@
     echo("<br>Doctor_ID: ".$_REQUEST["doctor_id"]);
     /*$_SESSION['try_name'] = $_POST['patient_name'];*/
   }
-
- 
-
- 
 ?>
-
 
 <html>
     <head>
       <title>Creating a new study</title>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     </head>
   <body>
     <h3>Inserting into DB</h3>
@@ -37,84 +33,106 @@
             echo("<p>INSERTed</p>");
             $sql = "SELECT number FROM ist173065.Request WHERE `date`= '" . $date_now . "'";
             $stmt = $connection->prepare($sql);
-            $stmt-> execute();
+            $stmt->execute();
             
             if($stmt == TRUE)
             {
                 echo("TRUE<br>");
                 $last_id = $stmt->fetchColumn();
-                
-/*                $last_id = $stmt->lastInsertId();*/
                 echo "New record created successfully. Last inserted ID is: " . $last_id;
                 
             }else{
-                echo("Error: " . $sql . "<br>" . $conn->error);
+                echo("Error: " . $sql . "<br>" . $stmt->error);
+
             }
-            echo("<h4>POPOPO</h4>");
-            $connection = null;
-
             
-    /*        
-            foreach ($_REQUEST as $exam_info => $value)
+            echo("<h4>going for the devices now</h4>");
+
+            $sql = "SELECT DISTINCT manufacturer FROM ist173065.Device";
+            $stmt = $connection->prepare($sql);
+            $stmt->execute();
+            echo("<h4>going for the devices now</h4>");
+            if($stmt == TRUE)
             {
-                echo("<p>$exame_info = $value</p>");
+                echo ("<h3>Available Devices</h3>");
+                echo("<table border=\"1\">");
+                echo("<tr><td>manufacturer</td></tr>");
+                
+                /*$cnt = 0;*/
+                foreach($stmt as $row)
+                {
+                    echo("</td><td>");
+                    echo($row['manufacturer']);
+                    echo("</td></tr>");
+                    /*$manuf_array[$cnt] = $row['manufacturer'];
+                    $model_array[$cnt] = $row['model'];
+                    $device_array[$cnt] = ( $row['manufacturer'] . " - " . $row['model']);
+                    $cnt = $cnt+1;*/
+                }
+                echo("</table>");
+            }else{
+                echo("Error: " . $sql . "<br>" . $stmt->error);
+
             }
-
-            try
-            {
-
-                $sql = "INSERT INTO ist173065.`Request` (`number`, `patient_id`, `doctor_id`, `date`) VALUES (null, '" . $_REQUEST['patient_id'] . "' , '" . $_REQUEST['doctor_id'] . "' , NOW() )";
-
-                
-                $patient_ID =  $_REQUEST['patient_id'];
-                $doctor_ID = $_REQUEST['doctor_id'];
-                $date = date('Y-m-d H:i:s');
-
-                $stmt= $connection->prepare("INSERT INTO ist173065.`Request` (patient_id, doctor_id, `date`) VALUES (:patient_ID, :doctor_ID, :`date`)");
-
-                
-                if($stmt == false){
-                    echo("<p>prepare returned FALSE");
-                    echo("</p>");   
-                }
-
-                $stmt->bindParam(':patient_ID', $patient_ID);
-                $stmt->bindParam(':doctor_ID', $doctor_ID);
-                $stmt->bindParam(':date', $date);
-
-
-
-
-                $account_number = $_REQUEST['account_number'];
-                $branch_name = $_REQUEST['branch_name'];
-                $balance = $_REQUEST['balance'];
-                $stmt = $connection->prepare("INSERT INTO account
-                VALUES (:account_number, :branch_name, :balance)");
-                $stmt->bindParam(':account_number', $account_number);
-                $stmt->bindParam(':branch_name', $branch_name);
-                $stmt->bindParam(':balance', $balance);
-                $stmt->execute();
-
-
-                if( $stmt->bind_param("iis", $patient_ID , $doctor_ID , $date) ){
-                    $patient_ID =  $_REQUEST['patient_id'];
-                    $doctor_ID = $_REQUEST['doctor_id'];
-                    $date = date('Y-m-d H:i:s');
-                    echo("<p>bind_param OK");
-                    echo("</p>");
-                }else{
-                    echo("<p>Error: bind_param");
-                    echo("</p>");
-                }
-    */
             
-            
-
         ?>
+
+        <form action="newSeries.php" method="post">
+            <h3>Create new study</h3>
+            <p>Insert short description for this study: <input type="text" name="study_description"/></p>
+            
+            <select onchange="get_models()" id="manuf" name="manufacturer"/>
+                <option value=""  selected disabled>Device manufacturer</option>
+                <?php
+                    $result = sql_secure_query($connection, "SELECT DISTINCT manufacturer FROM ist173065.Device");
+
+                    foreach($result as $row1){
+                        echo("<option value=\"{$row1['manufacturer']}\"> {$row1['manufacturer']} </option>");
+                    }
+                ?>
+
+            </select>
+
+            <?php
+              /*  $sql = "SELECT DISTINCT model FROM ist173065.Device";
+                $stmt = $connection->prepare($sql);
+                $stmt->execute();*/
+            ?>
+
+            <select name="model">
+                <option selected="selected">Choose one</option>
+                <?php
+                    $result = sql_secure_query($connection, "SELECT * FROM ist173065.Device");
+                    foreach($result as $value2) { 
+                        echo("<option  class=\" hide_show {$value2['manufacturer']}\"  value=\"{$value2['model']}\"> {$value2['model']} </option>");
+                    }
+                ?>
+            </select> 
+
+            
+
+            <p><input type="submit" name="submit" value="Submit"/></p>
+        </form>
+
+        <?php $connection = null; ?>
+
         <p>Patient ID: <?php echo($_POST['patient_id']); ?> </p>
         <p>Doctor ID: <?php echo($_POST['doctor_id']); ?> </p>
         <p>date: <?php echo($date_now); ?> </p>
         <p>END OF FILE :D</p>
-  </body>
+    </body>
+
+
+    <script>
+        function get_models(){
+        var manuf = $("#manuf option:selected").val();
+        /*Show only models that this manufacturer has*/
+        $('.hide_show').prop('disabled', true);
+        /*Show all models that this manufacturer has*/
+        $('.' + manuf).prop('disabled', false);
+        $('[name=model]').val( '' );
+        
+        }
+    </script>
 
 </html>
